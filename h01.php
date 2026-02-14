860d9b42a079a8239c26e1df8bb36da15f5294d7
hploompuu-spec <hploompuu@hkhk.edu.ee>
2026-02-14 13:08:32 +0200

    poolik

--- h01.php
+++ h01.php
@@ -2,7 +2,23 @@
 /*
 	01 - PHP - Muutujad
 	Hannes Ploompuu
-	Haapsalu Kutsehariduskeskus
 	14.02.2026
 */
+//kolme muutuja väärtused
+    $nimi = "Hannes Ploompuu";
+    $aasta = "1981";
+    $tahtkuju = "Kaljukits";
+
+    echo "Minu nimi on $nimi,<br>ma olen sündinud  $aasta aastal<br>ja minu tähtkuju on $tahtkuju.<br>";
+
+//laul ja artist
+    $laul = "It's My Life";
+    $artist = "Dr. Alban";
+
+    echo $laul . " – " . $artist;
+
+//pilt
+
+    echo "(\(\<br>( -.-)<br>o_(\")(\")";
+
 ?>
